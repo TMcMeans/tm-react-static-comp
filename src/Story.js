24dbf0fd0455b0fa2d501f26cc372e81d5743116
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-09-04 12:24:55 -0600

    Refactor classes

--- src/Story.js
+++ src/Story.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Button from './Button';
+import Author from './Author';
 import './Story.css';
 
 const Story = (props) => {
   return (
     <div className='Story'>
-      <img src={props.img} />
+      <img src={`public/${props.story.img}`} />
       <div className='story-info'>
         <div>
-          <h3>{props.title}</h3>
-          <p>{props.desc}</p>
+          <h3>{props.story.title}</h3>
+          <p>{props.story.desc}</p>
         </div>
-        {/* create an Author component and add it here */}
-        {/* it will need properties of the author image, name, and the estimated reading time of the story */}
+        <Author 
+        key={props} 
+        authImg={props.story.authImg}
+        authName={props.story.authName}
+        estTime={props.story.estTime}
+        />
       </div>
     </div>
   )
 }
+
 
 export default Story;
